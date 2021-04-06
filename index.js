4a6ef90b929d51d3c97ea7386297534c300eb997
harishcalvin <36122478+harishcalvin@users.noreply.github.com>
2021-04-06 10:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navlinks');
-    const navLinks = document.querySelectorAll('.navlink');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-            //close burger
-            burger.classList.toggle('toggle');
-
-    }); 
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    //animation
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`
+        }
+    });
+    //close burger
+    burger.classList.toggle('toggle');
+}); 
 for (const navLink of navLinks) {
   navLink.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
